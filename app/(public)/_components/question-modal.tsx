3650e69e64bec5ec39168477c272ee1e29e1c692
ahmed <ahmedaltokey320@gmail.com>
2024-11-16 22:20:19 +0200

    Edit

--- app/(public)/_components/question-modal.tsx
+++ app/(public)/_components/question-modal.tsx
@@ -79,9 +79,11 @@ export default function QuestionModal({
 
     const renderAnswerItems = (answerItems?: AnswerItem[]) =>
         answerItems?.map((item) => (
-            <div key={item.id}>
+            <div key={item.id} className="p-4" >
                 {item.type === "text" && (
-                    <p className="text-2xl leading-normal">
+                    <p
+                        className={`leading-normal`}
+                    >
                         {item.value
                             ?.split(/(https?:\/\/[^\s]+)/g)
                             .map((part, index) =>
@@ -95,7 +97,7 @@ export default function QuestionModal({
                                             target="_blank"
                                             rel="noopener noreferrer"
                                         >
-                                            من هنا 
+                                            من هنا
                                         </a>
                                     </div>
                                 ) : (
@@ -147,9 +149,6 @@ export default function QuestionModal({
                                 <TiArrowUpThick />
                             </div>
                         )}
-                        <p className="leading-loose font-medium">
-                            {node.title || node.label}
-                        </p>
                         <div className="flex flex-col text-center gap-4">
                             {renderAnswerItems(node.answer)}
                         </div>
